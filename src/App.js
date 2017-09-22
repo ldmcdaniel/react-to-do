71544b8446918968085e6272de9fcc9697d00ec7
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2017-09-21 22:37:58 -0500

    Added initial files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="header">
+          <h1>To Do</h1>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
