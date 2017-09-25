5723b4eeb20629e13a62d0d745fba8cb89de66a5
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2017-09-25 17:34:34 -0500

    Updated elements

--- src/App.js
+++ src/App.js
@@ -1,21 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bulma/css/bulma.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      urls: [],
+      url: '',
+    };
+    this.submitUrl = this.submitUrl.bind(this);
+    this.handleUrl = this.handleUrl.bind(this);
+    this.clearUrl = this.clearUrl.bind(this);
+  }
+  submitUrl(e) {
+    e.preventDefault();
+    fetch(this.state.url)
+    .then((response) => response.text())
+    .then((data) => {
+      console.log(data);
+      const dataArray = Object.values(data);
+      return dataArray.map((url) => {
+        const urls = this.state.urls;
+        urls.push(url);
+        this.setState({ urls: urls });
+      });
+    })
+    .catch((error) => console.log(error));
+  }
+  handleUrl(e) {
+    this.setState({ url: e.target.value });
+  }
+  clearUrl(e) {
+    this.setState({ url: '' });
+  }
   render() {
+    const urlsArray = this.state.urls;
+    const urls = urlsArray.map((url) => <li><a className="tag is-success" href={url}>{url}</a></li> );
     return (
-      <div className="App">
-        <div className="header">
-          <h1>To Do</h1>
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-            <li>Item 4</li>
-            <li>Item 5</li>
-          </ul>
+      <div className="App columns">
+        <div className="column"></div>
+        <div className="column is-three-quarters">
+          <div className="field">
+            <label className="label">Enter URL</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                placeholder="https://"
+                value={ this.state.url }
+                onChange={ this.handleUrl }
+              />
+            </div>
+          </div>
+          <div className="field is-grouped">
+            <div className="control">
+              <button className="button is-primary" onClick={ this.submitUrl }>Submit</button>
+            </div>
+            <div className="control">
+              <button className="button is-link">Cancel</button>
+            </div>
+          </div>
+          <div className="content">
+            <h1>URLs found on {this.state.url} </h1>
+            <ul>{urls}</ul>
+          </div>
         </div>
+        <div className="column"></div>
       </div>
     );
   }
